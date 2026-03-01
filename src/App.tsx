43e76511fd318a372f1e715aee8e7fed336c485f
Jonh Look <Axel Lopez Cornejo>
2026-03-01 14:16:13 -0600

    Cambios antes de integrar lo nuevo (ya esta frontend)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App(){
   return (
     <>
-    <h1 className="bg-amber-200">Equipo tilines project 😎</h1>
+    <h1 className="bg-amber-200">Equipo de los  tilines project 😎</h1>
     </>
   )
 }
